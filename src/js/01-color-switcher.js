b190c80978f76c1cdfd68a7b4933b2f031fa86d8
coll16 <126190508+coll16@users.noreply.github.com>
2023-07-22 00:43:36 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const btnStart = document.querySelector('button[data-start]')
+const btnStop = document.querySelector('button[data-stop]')
+const body = document.querySelector('body')
+
+let intervalId = ''
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, '0')}`
+}
+
+btnStart.addEventListener('click', () => {
+  intervalId = setInterval(() => {
+    btnStart.disabled = true
+    body.style.backgroundColor = getRandomHexColor()
+  }, 1000);
+});
+
+btnStop.addEventListener('click', () => {
+  clearInterval(intervalId)
+  btnStart.disabled = false
+  body.style.backgroundColor = ''
+});
 
